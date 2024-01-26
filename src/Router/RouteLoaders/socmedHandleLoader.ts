024047d6b603381b67a6301534c57c736a8eb8cf
leicester70 <lestercheong70@gmail.com>
2024-01-27 03:15:11 +0800

    added route, /socmed-handle, with loader

---
+++ src/Router/RouteLoaders/socmedHandleLoader.ts
@@ -0,0 +1,4 @@
+export const socmedHandleLoader = () => {
+
+    return undefined;
+}
--- src/Router/router.ts
+++ src/Router/router.ts
@@ -1,13 +1,19 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 import { LandingView } from "../Views/LandingView";
+import { socmedHandleLoader } from "./RouteLoaders/socmedHandleLoader";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: LandingView,
-  },
-  ...ProtectedRoutes.map((route: RouteObject) => {
-    return route;
-  }),
+    {
+        path: "/",
+        Component: LandingView,
+    },
+    {
+        path: "/socmed-handle",
+        Component: LandingView,
+        loader: socmedHandleLoader()
+    },
+    ...ProtectedRoutes.map((route: RouteObject) => {
+        return route;
+    }),
 ]);
