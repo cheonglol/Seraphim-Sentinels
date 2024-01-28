4654da91f71f339d263bf95cf88c54877e7b9b56
leicester70 <lestercheong70@gmail.com>
2024-01-28 22:44:04 +0800

    error view

--- src/Router/router.ts
+++ src/Router/router.ts
@@ -3,16 +3,22 @@ import { ProtectedRoutes } from "./ProtectedRoutes";
 import { LandingView } from "../Views/LandingView";
 import { socmedHandleLoader } from "./RouteLoaders/socmedHandleLoader";
 import { SocmedReferredView } from "../Views/SocmedReferredView";
+import { ErrorView } from "../Views/ErrorView";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: LandingView,
+        ErrorBoundary: ErrorView
     },
+    // External handle
     {
         path: "/socmed-handle",
         Component: SocmedReferredView,
         loader: socmedHandleLoader,
+    },
+    {
+        path: "/games"
     },
     ...ProtectedRoutes.map((route: RouteObject) => {
         return route;
