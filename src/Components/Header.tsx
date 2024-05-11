7e54fcac86d8fe4e5472cd8d936b803650e650fd
Cheong L. <82318965+leicester70@users.noreply.github.com>
2024-05-12 07:31:45 +0800

    add crap

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
     ]
 
     return (
-        <div className="p-4">
+        <div className="py-4 max-w-[90vw] m-auto">
             <Menubar
                 start={
                     <a
--- src/Views/LandingView.tsx
+++ src/Views/LandingView.tsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import { BasicLayout } from '../Layouts/BasicLayout'
+import { Card } from 'primereact/card'
+import { Button } from 'primereact/button'
 
 export const LandingView = () => {
     return (
         <BasicLayout
             mainContent={
-                <>
-                    hey look, its my Twitch Channle,
-                    https://www.twitch.tv/leicestershire70
-                </>
+                <div className="grid grid-cols-1 sm:grid-cols-0 gap-4 max-w-[90vw] m-auto">
+                    <Card className="bg-slate-600">
+                        <h1>My Home Feed</h1>
+                        <p>
+                            I post whatever the hell i want here on <br />
+                            <a
+                                className="font-black text-blue-500 text-[200%]"
+                                href="https://cheong.lol"
+                            >
+                                C'MON THIS IS MY OWN FUCKING SITE!
+                                <br /> LEAVE ME BE!
+                            </a>
+                            <br />
+                            (you may not have one, but keep ur judgment to
+                            yourself)
+                        </p>
+                        <Card title="My Twitch 📹🎥🗣️💯" className="mt-6">
+                            <div>
+                                <Button
+                                    link
+                                    onClick={() =>
+                                        window.open(
+                                            'https://www.twitch.tv/cheonglol70',
+                                            '_blank'
+                                        )
+                                    }
+                                />
+                                <a
+                                    href="https://www.twitch.tv/cheonglol70"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="p-button font-bold text-xl"
+                                >
+                                    Go Navigate Yourself to my twitch chhannel
+                                    please <br /> (pls support me 💖💖)
+                                </a>
+                            </div>
+                        </Card>
+                    </Card>
+                </div>
             }
         />
     )
