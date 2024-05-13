895e5d6df00aa7b6223a065dc79d15a3f2008a8a
Cheong L. <82318965+leicester70@users.noreply.github.com>
2024-05-14 06:52:22 +0800

    `TODO:` YOU KNOW WHAT TO DO

---
+++ src/Components/BlogContent.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import { connect } from 'react-redux'
+
+// export const BlogContent = (props) => {
+//   return (
+//     <div>BlogContent</div>
+//   )
+// }
+
+// const mapStateToProps = (state) => ({})
+
+// const mapDispatchToProps = {}
+
+// export default connect(mapStateToProps, mapDispatchToProps)(BlogContent)
