7865e2647b30004172920558b716c64da1c639f4
Lester Cheong <lestercheong70+github@outlook.com>
2024-05-23 22:47:02 +0800

    init

---
+++ src/Views/GettingStartedView.tsx
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+interface Props {
+  children?: any;
+}
+
+export const GettingStartedView = (props: Props) => {
+  return <div>GettingStartedView</div>;
+};
