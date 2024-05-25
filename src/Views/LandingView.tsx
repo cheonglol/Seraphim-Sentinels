0d351a6624539871cb1c55039c86fd14afe8267a
Lester Cheong <lestercheong70+github@outlook.com>
2024-05-26 06:15:08 +0800

    thomas pull this shit (im so tired ngl)

--- src/Views/LandingView.tsx
+++ src/Views/LandingView.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import { BasicLayout } from "../Layouts/Base/BasicLayout";
 import RetirementPlanDashboard from "./RetirementPlanDashboard";
+import { getAiResponseHistory } from "../APIs/JigsawStackAPIs/PromptEngine_API";
 
 export const LandingView = () => {
+  const [imTired, setImTired] = useState<"tired" | "imActuallySoTired">(
+    "tired"
+  );
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getAiResponseHistory();
+      alert(JSON.stringify(data));
+      setImTired("imActuallySoTired");
+    };
+    if (imTired !== "imActuallySoTired") {
+      fetchData();
+    }
+  }, []);
+
   // TODO: we need to change this or at least redircet
   return <BasicLayout mainContent={<RetirementPlanDashboard />} />;
 };
