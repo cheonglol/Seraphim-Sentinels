1ed6c21755f3b9fd00ece2fc69622d1226224817
Lester Cheong <lestercheong70+github@outlook.com>
2024-05-25 19:57:56 +0800

    it works okay i dont want to look at this anymore what a fucking waste of time

--- src/APIs/Supabase/test.ts
+++
@@ -1,2 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabase = createClient(`${process.env.REACT_APP_SUPABASE_POSTGRESQL_DB_URL}`, `${process.env.REACT_APP_SUPABASE_ACCESS_TOKEN}`)
