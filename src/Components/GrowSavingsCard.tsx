3053a96fb13a7adc516a2d5b7dcfbf12706e92f4
hyosus <shaniceyeong@gmail.com>
2024-05-25 20:52:45 +0800

    create grow-savings view

--- src/Components/GrowSavingsCard.tsx
+++ src/Components/GrowSavingsCard.tsx
@@ -11,7 +11,7 @@ export const GrowSavingsCard = ({
 }: GrowSavingsCardProps) => {
   return (
     <div className="rounded-xl bg-white mt-6 bg-opacity-15 backdrop-filter backdrop-blur flex flex-col justify-between">
-      <h1 className="pt-4 pl-4 pr-4">{header}</h1>
+      <h1 className="pt-4 pl-4 pr-4 font-semibold">{header}</h1>
       <p className="pt-2 pl-4 pr-4 font-light text-lg">{paragraph}</p>
       <p className="text-right pt-2 pl-4 pr-4 pb-4 font-medium text-xl">
         {interestRate}% p.a
