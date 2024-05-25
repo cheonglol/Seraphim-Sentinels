256744ea467fa3e90b99268091fe6580e9ce4dc6
thomas <thomastmx@hotmail.com>
2024-05-26 02:37:44 +0800

    added APIs

--- src/Views/LandingView.tsx
+++ src/Views/LandingView.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { Card } from "primereact/card";
 import ReactPlayer from "react-player";
 import { BasicLayout } from "../Layouts/Base/BasicLayout";
-import { getCustomerList } from "../APIs/Supabase/CustomerAPI";
+import {
+  getCustomerList,
+  getCustomerList_ByCustomerID,
+} from "../APIs/Supabase/Customer_Table_APIs";
+import { getTransactionAPI_ByCustomerID } from "../APIs/Supabase/Transaction_Table_APIs";
 
 // TODO: move each post as its own component under `src\Components\blog` so i dont make this fucking landing page SO LONG TO READ wtf
 
@@ -14,7 +18,8 @@ export const LandingView = () => {
     if (!isDataFetched) {
       const fetchData = async () => {
         try {
-          const data = await getCustomerList();
+          //const data = await getCustomerList();
+          const data = await getTransactionAPI_ByCustomerID(1);
           alert(JSON.stringify(data));
         } catch (error) {
           alert(JSON.stringify(error));
