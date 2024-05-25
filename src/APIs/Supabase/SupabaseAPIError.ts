50bfa030018cd2d13cc1e08a255c06b4182ea3cc
Lester Cheong <lestercheong70+github@outlook.com>
2024-05-25 22:37:14 +0800

    REFER to working copy

---
+++ src/APIs/Supabase/SupabaseAPIError.ts
@@ -0,0 +1,4 @@
+export interface Supabase_API_Error {
+    message: string;
+    hint: string;
+}
--- src/Views/LandingView.tsx
+++ src/Views/LandingView.tsx
@@ -2,33 +2,27 @@ import { useEffect, useState } from "react";
 import { Card } from "primereact/card";
 import ReactPlayer from "react-player";
 import { BasicLayout } from "../Layouts/Base/BasicLayout";
-import { createClient } from "@supabase/supabase-js";
+import { getCustomerList } from "../APIs/Supabase/CustomerAPI";
 
 // TODO: move each post as its own component under `src\Components\blog` so i dont make this fucking landing page SO LONG TO READ wtf
 
 export const LandingView = () => {
-  const supabaseUrl = process.env.REACT_APP_SUPABASE_PROJECT_URL;
-  const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
   const [isDataFetched, setIsDataFetched] = useState<boolean>(false);
+  //   const [first, setfirst] = useState<unknown>("NOTHING");
 
   useEffect(() => {
-    const fetchData = async () => {
-      let supabase = createClient(supabaseUrl as string, supabaseKey as string);
-      if (isDataFetched) {
-        return;
-      }
-      try {
-        const { data, error } = await supabase.from("Customer").select("*");
-        console.log(data);
-        //alert(data);
-        //alert(JSON.stringify(error));
-      } catch (error) {
-        console.log(error);
-      }
-      setIsDataFetched(true);
-      return;
-    };
-    fetchData();
+    if (!isDataFetched) {
+      const fetchData = async () => {
+        try {
+          const data = await getCustomerList();
+          alert(JSON.stringify(data));
+        } catch (error) {
+          alert(JSON.stringify(error));
+        }
+        setIsDataFetched(true);
+      };
+      fetchData();
+    }
   }, []);
 
   return (
